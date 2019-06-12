97f644ea9a25c533e5591c6419e31f9392894a8c
Razym209 <47150855+Razym209@users.noreply.github.com>
2019-06-12 16:55:55 +0300

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,18 +2,13 @@
   belongs_to :question
   scope :correct, -> { where(correct: true) }
   validates :body, presence: true
-  validates :question_id, presence: true,
   validate :validate_answers_count_max
   private
 
   def validate_answers_count_max
-    if answers_count_max?
+    if question.answers.count >= Setting.max_answers
       error = "must include from #{Setting.min_answers} to #{Setting.max_answers} answers"
       errors.add(:question_id, error)
     end
-  end
-
-  def answers_count_max?
-    question.answers.count >= Setting.max_answers
   end
 end
