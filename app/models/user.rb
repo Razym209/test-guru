e7ddc07631f728b3f32dc56688ccb238bc1b20f6
Razym209 <47150855+Razym209@users.noreply.github.com>
2019-06-24 22:40:39 +0300

    9 Zadanie

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ApplicationRecord
   
   has_many :test_passages
   has_many :tests, through: :test_passages
+  
   has_many :role_users, dependent: :destroy
   has_many :roles, through: :role_users
-  has_many :test_authors
-  has_many :created_tests, through: :test_authors, source: :test
-
+ 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
 
