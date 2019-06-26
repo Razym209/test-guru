c17f5833f09329c43de5b70ca185167bdb880a41
Razym209 <47150855+Razym209@users.noreply.github.com>
2019-06-26 12:52:04 +0300

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_041353) do
+ActiveRecord::Schema.define(version: 2019_06_26_075805) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -78,8 +78,10 @@ ActiveRecord::Schema.define(version: 2019_06_24_041353) do
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
